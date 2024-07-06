4fac886ffe118c1a2302c26ee0cf449498ce98f3
Joanzam <joanzam.com@gmail.com>
2024-07-06 08:08:33 +0000

    ejercicio finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const simbolos = ["❤️", "♣️", "♦️", "♠️"];
+
+  const numeroRandom = numberR => Math.floor(Math.random() * numberR.length);
+  const simboloRandom = simbolR => Math.floor(Math.random() * simbolR.length);
+  let simbolo = simboloRandom(simbolos);
+  let numero = numeroRandom(numeros);
+
+  document.getElementById("numero").innerHTML = numeros[numero];
+  document.getElementById("simbolo").innerHTML = simbolos[simbolo];
+  document.getElementById("simboloDown").innerHTML = simbolos[simbolo];
 };
